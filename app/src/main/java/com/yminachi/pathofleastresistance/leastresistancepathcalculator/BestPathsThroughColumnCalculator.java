ec652f36dea24e905f0477ea6cf5d7f98f90dd80
Yasmine <yminachi@pillartechnology.com>
2016-06-27 11:21:36 -0400

    best paths through column

---
+++ app/src/main/java/com/yminachi/pathofleastresistance/leastresistancepathcalculator/BestPathsThroughColumnCalculator.java
@@ -0,0 +1,35 @@
+package com.yminachi.pathofleastresistance.leastresistancepathcalculator;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BestPathsThroughColumnCalculator {
+    private AdjacentCellPathPicker adjacentCellPathPicker;
+    private BestPathThroughCellCalculator bestPathThroughCellCalculator;
+
+    public BestPathsThroughColumnCalculator(AdjacentCellPathPicker adjacentCellPathPicker, BestPathThroughCellCalculator bestPathThroughCellCalculator){
+        this.adjacentCellPathPicker = adjacentCellPathPicker;
+        this.bestPathThroughCellCalculator = bestPathThroughCellCalculator;
+    }
+
+    public Map<Integer, Path> getBestPathsForColumn(double[] column, Map<Integer, Path> previousBestPathsForColumn, double maxTotal){
+        Map<Integer, Path> bestPathsForColumn = new HashMap<>();
+
+        for (int rowIndex = 0; rowIndex < column.length; rowIndex++){
+            Path pathThroughCell = getBestPathThroughCell(previousBestPathsForColumn, rowIndex, column[rowIndex]);
+
+            if (pathThroughCell != null && pathThroughCell.getPathTotal() <= maxTotal){
+                bestPathsForColumn.put(rowIndex, pathThroughCell);
+            }
+        }
+
+        return bestPathsForColumn;
+    }
+
+    private Path getBestPathThroughCell(Map<Integer, Path> previousBestPathsForColumn, int rowIndex, double cellValue){
+        List<Path> adjacent = adjacentCellPathPicker.pickAdjacentPaths(previousBestPathsForColumn, rowIndex);
+
+        return adjacent.isEmpty() ? null : bestPathThroughCellCalculator.getBestPathThroughCell(adjacent, rowIndex, cellValue);
+    }
+}
